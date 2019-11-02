bcc80f956040820154b4915ae1793602c7dec95e
Bob <bob.e.baxter@gmail.com>
2019-11-02 11:58:23 -0500

    candy class properties added

---
+++ CandyMarket.Api/DataModels/UserCandy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyMarket.Api.DataModels
+{
+    public class UserCandy : Candy
+    {
+        public DateTime DateReceived { get; set; }
+    }
+}
